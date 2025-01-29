1e29f0f9b8f4a7e7dda8043d229bce9095abf3a5
WIN-IIOAPJPSVCU\Administrator <hasanuxaman.rony@gmail.com>
2025-01-29 20:06:40 +0600

    Login Successfull

--- FaixariT/Controllers/AuthController.cs
+++ FaixariT/Controllers/AuthController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FaixariT.HRMS.DBModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace FaixariT.Controllers
 {
@@ -7,6 +14,81 @@ namespace FaixariT.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
 
+        public AuthController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null || user.PasswordHash != request.Password)
+                return Unauthorized(new { message = "Invalid email or password" });
+
+            var token = GenerateJwtToken(user);
+            return Ok(new { token, userId = user.UserId, name = user.FirstName });
+        }
+
+        private string GenerateJwtToken(Users user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("UserId", user.UserId.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpGet("menu")]
+        public async Task<IActionResult> GetMenu()
+        {
+            var modules = await _context.AppModule
+                .Include(m => m.AppMenu)
+                .ThenInclude(menu => menu.AppSubMenu)
+                .ToListAsync();
+
+            var response = modules.Select(module => new
+            {
+                module.ModuleId,
+                module.ModuleName,
+                Menus = module.AppMenu.Select(menu => new
+                {
+                    menu.MenuId,
+                    menu.MenuName,
+                    menu.Url,
+                    SubMenus = menu.AppSubMenu.Select(sub => new
+                    {
+                        sub.SubMenuId,
+                        sub.SubMenuName,
+                        sub.Url
+                    }).ToList()
+                }).ToList()
+            });
+
+            return Ok(response);
+        }
+    }
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
--- FaixariT/Program.cs
+++ FaixariT/Program.cs
@@ -1,5 +1,8 @@
 using FaixariT.HRMS.DBModels;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Configure JWT Authentication
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
 
 var app = builder.Build();
 
